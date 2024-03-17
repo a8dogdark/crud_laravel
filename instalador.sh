8af5da19c7f857bdeccbfd28f8da0d3c53c86ade
dogdark <42787966+a8dogdark@users.noreply.github.com>
2024-03-17 14:04:42 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -1,106 +1,146 @@
 #!/bin/sh
 clear
-echo "agregamos php 8 al sistema"
+
+echo "******************************"
+echo "* agregamos php 8 al sistema *"
+echo "******************************"
 sleep 3
 sudo add-apt-repository ppa:ondrej/php
 
-echo "actualizamos el sistema"
+echo "***************************"
+echo "* actualizamos el sistema *"
+echo "***************************"
 sleep 3
 #actualizamos el sistema
 sudo apt update -y
 
-echo "agregamos los paquetes requeridos"
+echo "*************************************"
+echo "* agregamos los paquetes requeridos *"
+echo "*************************************"
 sleep 3
 #agregamos las dependencias requeridas
 sudo apt install curl php8.2-cli php8.2-mbstring git unzip wget -y
 
-echo "descargamos xampp"
+echo "*********************"
+echo "* descargamos xampp *"
+echo "*********************"
 sleep 3
 #descargamos xampp desde su web oficial
 wget https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/8.2.12/xampp-linux-x64-8.2.12-0-installer.run
 
-echo "descargamos composer"
+echo "************************"
+echo "* descargamos composer *"
+echo "************************"
 sleep 3
 #descargamos composer
 wget https://getcomposer.org/download/2.7.2/composer.phar
 
-echo "cambiamos permisos a xampp y composer"
+echo "*****************************************"
+echo "* cambiamos permisos a xampp y composer *"
+echo "*****************************************"
 sleep 3
 # cambiamos los permisos a los archivos
 sudo chmod +x xampp-linux-x64-8.2.12-0-installer.run
 sudo chmod +x composer.phar
 
-echo "movemos composer"
+echo "********************"
+echo "* movemos composer *"
+echo "********************"
 sleep 3
 # movemos composer
 sudo mv composer.phar /usr/local/bin/composer
 
-echo "agregamos composer al bashrc"
+echo "********************************"
+echo "* agregamos composer al bashrc *"
+echo "********************************"
 sleep 3
 #agregamos composer al path
 echo 'export PATH="~/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
 
-echo "activamos composer"
+echo "**********************"
+echo "* activamos composer *"
+echo "**********************"
 sleep 3
 sudo -s source ~/.bashrc
 
-echo "instalamos xampp"
+echo "********************"
+echo "* instalamos xampp *"
+echo "********************"
 sleep 3
 #instalamos xampp
 sudo ./xampp-linux-x64-8.2.12-0-installer.run
 
-echo "detenemos xampp"
+echo "*******************"
+echo "* detenemos xampp *"
+echo "*******************"
 sleep 3
 #detenemos el servidor
 sudo /opt/lampp/lampp stop
 
-echo "creamos enlaces simbolicos de xampp"
+echo "***************************************"
+echo "* creamos enlaces simbolicos de mysql *"
+echo "***************************************"
 sleep 3
 #creamos los enlaces simbolicos
 sudo ln -s /opt/lampp/bin/mysql /usr/bin
 
-echo "instalamos nodejs"
+echo "*********************"
+echo "* instalamos nodejs *"
+echo "*********************"
 sleep 3
 #instalamos node js
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 
-echo "agregamos instalador de laravel a composer"
+echo "**********************************************"
+echo "* agregamos instalador de laravel a composer *"
+echo "**********************************************"
 sleep 3
 #configuramos composer para laravel
 composer global require laravel/installer
 
-echo "eliminamos la carpeta htdocs de xampp"
+echo "*****************************************"
+echo "* eliminamos la carpeta htdocs de xampp *"
+echo "*****************************************"
 sleep 3
 #eliminamos los archivos de la carpeta htdocs
 sudo rm -R /opt/lampp/htdocs
 
-echo "creamos la carpeta htdocs de xampp nuevamente"
+echo "*************************************************"
+echo "* creamos la carpeta htdocs de xampp nuevamente *"
+echo "*************************************************"
 sleep 3
 #creamos la carpeta htdocs nuevamente
 sudo mkdir /opt/lampp/htdocs
 
-echo "cambiamos permisos de escritura a htdocs de xampp"
+echo "*****************************************************"
+echo "* cambiamos permisos de escritura a htdocs de xampp *"
+echo "*****************************************************"
 sleep 3
 
 #cambiamos los permisos de la carpeta htdocs de xampp
 sudo chmod 777 -R /opt/lampp/htdocs
 
-echo "nos movemos a la carpeta a htdocs"
+echo "*************************************"
+echo "* nos movemos a la carpeta a htdocs *"
+echo "*************************************"
 sleep 3
 #nos movemos a la carpeta htdocs
 cd /opt/lampp/htdocs
 
 #creamos el nuevo proyecto
-echo "creamos el nuevo proyecto de crud"
+echo "*************************************"
+echo "* creamos el nuevo proyecto de crud *"
+echo "*************************************"
 sleep 3
 laravel new crud
 
-echo " "
-echo "cambiamos los permisos de la carpeta crud"
-echo " "
+echo "*********************************************"
+echo "* cambiamos los permisos de la carpeta crud *"
+echo "*********************************************"
 
 sudo chmod 777 -R /opt/lampp/htdocs/crud
+
 
 
 
