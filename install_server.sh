c9346f26fd2c2570a7431e362374e7c0cd393b9f
a8dogdark <a8dogdark@gmail.com>
2024-03-17 23:03:03 -0300

    17-03-2024

--- install_server.sh
+++ install_server.sh
@@ -7,15 +7,24 @@ if [ -f "$pwd/slib.sh" ]; then
     # shellcheck disable=SC1091
     . "$pwd/slib.sh"
   # Download the slib (source: http://github.com/virtualmin/slib)
-  else
-    # We need HTTP client first
-    pre_check_http_client
-    $download "https://$upgrade_virtualmin_host_lib/slib.sh" >>"$log" 2>&1
-    if [ $? -ne 0 ]; then
-      echo "Error: Failed to download utility function library. Cannot continue. Check your network connection and DNS settings, and verify that your system's time is accurately synchronized."
-      exit 1
-    fi
+else
+    wget "https://raw.githubusercontent.com/a8dogdark/crud_laravel/main/slib.sh"
     chmod +x slib.sh
     # shellcheck disable=SC1091
     . ./slib.sh
-  fi
+fi
+
+clear
+
+printf "${CYAN}"
+printf "***************************************************"
+printf "* Bienvenido al instalador de paquetes de dogdark *"
+printf "* Instalación de                                  *"
+printf "* Servidor Xampp                                  *"
+printf "* Apache - PHP 8.2 - Mysql                        *"
+printf "***************************************************"
+sleep 5
+printf "${GREEN} "
+run_ok "sudo apt update -y" "Actualizando el sistema"
+
+exit 1
