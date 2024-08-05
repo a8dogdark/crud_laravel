f7d30a4bc3400f8b9eda21c326285b6264b02f3a
a8dogdark <a8dogdark@gmail.com>
2024-08-05 19:54:36 -0400

    05-08-2024 19:54

--- resources/views/admin/aarticulos.blade.php
+++ resources/views/admin/aarticulos.blade.php
@@ -1,8 +1,43 @@
 @extends('layout.layouta')
 @section('titleadmin','Artículos Publicados')
 @section('contentadmin')
-    <h2>Articulos</h2>
+    <div class="mbot-1">
+        <h2>Artículos</h2>
+    </div>
     <div>
-        <p>Estamos desde la vista principal de Artículos</p>
+        <table class="table mbot-1">
+            <tr>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>ACCION</th>
+            </tr>
+
+            @for ($i=0;$i<5;$i++)
+                <tr>
+                    <td>00</td>
+                    <td>NUEVO ARTICULO</td>
+                    <td>
+                        <a href="#">
+                            <i class='bx bxs-edit-alt btn__verde'></i>
+                        </a> 
+                        <a href="#">
+                            <i class='bx bxs-trash btn__rojo'></i>
+                        </a>
+                    </td>
+                </tr>
+            @endfor
+            
+            
+        </table>
+    </div>
+    <div class="pagination">
+        <ul>
+            @for ($a=0;$a<5;$a++)
+                <li>
+                    <a href="#">1</a>
+                </li>    
+            @endfor
+            
+        </ul>
     </div>
 @endsection
